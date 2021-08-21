ea35e98c8fc22d9f9b9690e6ef7a0bec2124f7dc
Lauren Judge <l.m.judge@hotmail.com>
2021-08-22 07:39:31 +1000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,22 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700;900&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.app {
+  min-height: 100vh;
 }
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBEec_ShUYMnxNv2A_qKQysEoJZLGjhRe4",
+  authDomain: "addressbook-569c3.firebaseapp.com",
+  projectId: "addressbook-569c3",
+  storageBucket: "addressbook-569c3.appspot.com",
+  messagingSenderId: "111267875095",
+  appId: "1:111267875095:web:37e37348cf70990d0d4f52"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 Vue.config.productionTip = false
 
